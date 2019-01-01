3395c573f36f850cdb1010f13f807ea4b0e2b731
Zoheir <zoheirkhonyagar@gmail.com>
2019-01-01 16:34:59 +0330

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,29 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Menu from '../components/menu'
+
+const Header = ({ siteTitle }) => (
+<header className="header-container container">
+	<div className="">
+		<nav>
+			<h1 style={{  }}>
+				<Link to="/" >
+					{siteTitle}
+				</Link>
+			</h1>
+			<Menu />
+		</nav>
+	</div>
+</header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/components/layout.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import './layout.css'
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div className="container">
+          {children}
+          <footer>
+            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout

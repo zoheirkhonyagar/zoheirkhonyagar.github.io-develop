db395e256f797723b0f6fb2724677c71e9ca758e
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2023-04-19 09:06:08 +0330

    There will be victorious

--- src/components/image.js
+++ src/components/image.js
@@ -17,7 +17,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "profile-small.jpeg" }) {
+        placeholderImage: file(relativePath: { eq: "profile-small.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 500) {
               ...GatsbyImageSharpFluid
